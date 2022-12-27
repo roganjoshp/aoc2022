5fc4e85a2cb89470c6de4614b9954f98946c7c01
Josh Pilkington <josh.pilkington@peak.ai>
2022-12-27 14:00:58 +0000

    Built dock

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -5,6 +5,12 @@ struct Dock {
     stacks: HashMap<usize, Stack>,
 }
 
+impl Dock {
+    fn new(stacks: HashMap<usize, Stack>) -> Self {
+        Dock { stacks: stacks }
+    }
+}
+
 #[derive(Debug)]
 struct Stack {
     crates: VecDeque<Crate>,
@@ -33,7 +39,7 @@ impl Crate {
     }
 }
 
-fn parse_file(file: &str) -> () {
+fn parse_file(file: &str) -> (Vec<Vec<char>>, Vec<String>) {
     let contents = fs::read_to_string(file).expect("Cannot open file");
     let split_contents: Vec<&str> = contents.split("\n").collect();
     let split_point = split_contents
@@ -48,8 +54,13 @@ fn parse_file(file: &str) -> () {
     // Build from the bottom up
     stacks.reverse();
 
+    println!("{:?}", &moves[1..]);
+    let moves: Vec<String> = moves.iter().map(|&x| x.split(" ").collect()).collect();
+    return (stacks, moves);
+}
+fn build_dock(initial_stacks: &Vec<Vec<char>>) -> Dock {
     // First find each of the stacks
-    let starter: Vec<(usize, char)> = stacks[1..]
+    let starter_crates: Vec<(usize, char)> = initial_stacks[1..]
         .iter()
         .flat_map(|layer| {
             layer
@@ -64,18 +75,21 @@ fn parse_file(file: &str) -> () {
 
     // Now build them up
 
-    let next_level = starter
+    let all_stacks = starter_crates
         .iter()
         .fold(HashMap::<usize, Stack>::new(), |mut map, val| {
             map.entry(val.0)
-                .and_modify(|stack| stack.crates.push_back(Crate::new(val.1)))
+                .and_modify(|stack| stack.add_crate(Crate::new(val.1)))
                 .or_insert(Stack::start_new(Crate::new(val.1)));
             map
         });
 
-    println!("{:?}", &next_level);
+    Dock::new(all_stacks)
 }
 
+// fn build_dock(stacks: )
+
 fn main() {
-    let input = parse_file("./d5_input.txt");
+    let (stacks, moves) = parse_file("./d5_input.txt");
+    let dock = build_dock(&stacks);
 }
